78fb43b673a38825ecc184e8c3d231aa25d30a9c
Stef van Dijk <stef@vandijkstef.nl>
2019-02-27 10:16:07 +0100

    Old examples

--- public/code.js
+++ public/code.js
@@ -1,6 +1,61 @@
-function init(data) {
-	console.log(data);
+class Minor {
+	constructor() {
+		this.students = [];
+	}
+
+	AddStudent(student) {
+		this.students.push(student);
+	}
 }
+
+class Person {
+	constructor(data) {
+		this.name = data['Student'];
+	}
+
+	SayHi() {
+		console.log('Hi');
+	}
+}
+
+class Student extends Person {
+	constructor(data) {
+		// this.name --> undefined
+		super(data); // -> new Person(data)
+		// this.name --> Naam bevatten
+	}
+
+	SayHi() {
+		super.SayHi();
+		console.log('Yo');
+	}
+}
+
+class Teacher extends Person {
+	constructor(data) {
+		this.name = data['Student'];
+	}
+}
+
+const minor = new Minor();
+function init(data) {
+	data.forEach((studentData) => {
+		minor.AddStudent(new Student(studentData));
+	})
+
+	console.log(minor);
+
+
+	// let students = [];
+	// data.forEach((student) => {
+	// 	students.push({
+	// 		name: student['Student']
+	// 	});
+	// })
+	// console.log(students);
+}
+
+
 
 // Minor
 	// Does it actually work?
